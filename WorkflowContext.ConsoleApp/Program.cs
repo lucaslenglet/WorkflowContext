9f065c4f861b886fc932ecb4aafc4fc1e455c33e
LENGLET Lucas <lucas.lenglet.ll@gmail.com>
2024-11-09 16:54:19 +0100

    Initial commit

---
+++ WorkflowContext.ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WorkflowContext.ConsoleApp;
+
+await new ServiceCollection()
+    .AddSingleton<TimeProvider>(_ => TimeProvider.System)
+    .AddTransient<MessagePrinter>()
+    .BuildServiceProvider()
+    .GetRequiredService<MessagePrinter>()
+    .PrintAsync();
---
+++ WorkflowContext.ConsoleApp/Shared.cs
@@ -0,0 +1,34 @@
+﻿using CSharpFunctionalExtensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WorkflowContext.ConsoleApp;
+
+static class TimeSteps
+{
+    // Requires external dependencies via the IServiceProvider
+    public static async Task<UnitResult<TError>> GetDateUsingDependencies<TContext, TError>(TContext context)
+        where TContext : BaseContext<TError>, INow
+    {
+        // Fake I/O call to demonstrate that steps can be asynchronous
+        await Task.CompletedTask;
+
+        var timeProvider = context.Services.GetRequiredService<TimeProvider>();
+
+        context.Now = timeProvider.GetLocalNow().DateTime;
+
+        return UnitResult.Success<TError>();
+    }
+
+    // Has no external dependency
+    public static UnitResult<TError> GetDateWithoutServiceProvider<TError>(INow context)
+    {
+        context.Now = new DateTime(2024, 9, 11, 20, 00, 23);
+
+        return UnitResult.Success<TError>();
+    }
+}
+
+interface INow
+{
+    public Maybe<DateTime> Now { get; set; }
+}
